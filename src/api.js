6aebe43fd0af5aa8ee3018842cb171bad4d181ba
Charles Louis <charleslouis1234@gmail.com>
2023-09-30 16:50:14 +0530

    weather app created in one commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8b1bef4c3bmsh0109c1e973646d0p1f7e89jsnbd6bc4a9133c',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+
+export const WEATHER_API_KEY = "2f95e7ba93d210673070d9b23b416c71";
